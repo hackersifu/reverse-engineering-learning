7c38a8650baf320a63f33f87afba813d01c55f3c
jwmckiddy31@gmail.com <jwmckiddy31@gmail.com>
2025-05-12 15:24:22 -0700

    Update to conda

--- LLM4Decompile-Testing/setup.sh
+++ LLM4Decompile-Testing/setup.sh
@@ -2,7 +2,7 @@ git clone https://github.com/albertan017/LLM4Decompile.git
 cd LLM4Decompile
 
 # Use Conda as recommended
-conda create -n llm4decompile python=3.9 -y
+conda create -n llm4decompile python=3.10 -y
+conda init
 conda activate llm4decompile
-
 pip install -r requirements.txt
