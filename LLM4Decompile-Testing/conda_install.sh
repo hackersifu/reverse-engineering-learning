574676a4f03c41d9b9f079755bd159814041326e
jwmckiddy31@gmail.com <jwmckiddy31@gmail.com>
2025-05-12 12:15:52 -0700

    conda install script

---
+++ LLM4Decompile-Testing/conda_install.sh
@@ -0,0 +1,13 @@
+# Download Miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Run installer
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# Follow the prompts (press Enter to accept defaults)
+
+# Reload shell
+source ~/.bashrc
+
+# Test
+conda --version
